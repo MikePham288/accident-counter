694a8d6c16a976cbf67a932c5109dae1f4a8039f
Mike Pham <mikep@xello.world>
2024-06-01 04:06:36 -0400

    chore: React Exercise for component state

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(count);
+  
+  useEffect(() => {
+    setInput(count);
+  }, [count])
+  
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count }</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
+        <button onClick={() => setCount(count => count - 1)}>➖ Decrement</button>
         <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => setCount(count => count + 1)}>➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form onSubmit={(e) => { e.preventDefault();  setCount(input)}}>
+          <input type="number" value={input} onChange={($event) => setInput($event.target.valueAsNumber)}/>
           <button type="submit">Update Counter</button>
         </form>
       </div>
